b5d77142837d11e72d9924a46e3942785691cf93
leothomazelli <leonardothomazellif@gmail.com>
2021-10-06 18:40:04 -0300

    exercícios dart

---
+++ dart/basico/Exercicio01.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  var valorArea = triangulo(4,4);
+  print("A área do triangulo é: $valorArea");
+}
+//(base x altura) / 2
+
+
+triangulo(base, altura){
+  return (base*altura)/2;
+}
---
+++ dart/basico/Exercicio02.dart
@@ -0,0 +1,12 @@
+void main() {
+// Calcular e exibir a média aritmética de quatro valores quaisquer que serão digitados.
+
+  double num1 = 10;
+  double num2 = 20;
+  double num3 = 13;
+  double num4 = 51;
+
+  var total = (num1+num2+num3+num4)/4;
+  
+  print(total);
+}
